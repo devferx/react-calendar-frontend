2fe4323d8463183d4062db9cf1c923e9d94d9f71
devferx <ferkki@hotmail.com>
2021-12-08 11:38:37 -0400

    :sparkles: [ADD] PWA Support Config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { CalendarApp } from './CalendarApp';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './styles.css';
 
@@ -8,3 +10,5 @@ ReactDOM.render(
     <CalendarApp />,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();

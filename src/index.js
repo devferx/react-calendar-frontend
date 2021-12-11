50f95ccdcce4a724d4058e349de85af64eec6852
devferx <ferkki@hotmail.com>
2021-12-11 14:03:34 -0400

    :heavy_plus_sign: [ADD] Workbox

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { CalendarApp } from './CalendarApp';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './styles.css';
 
@@ -10,5 +9,3 @@ ReactDOM.render(
     <CalendarApp />,
   document.getElementById('root')
 );
-
-serviceWorkerRegistration.register();

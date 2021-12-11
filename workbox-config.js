303fbfc1a594f7fe6214528e497b3e455a462066
devferx <ferkki@hotmail.com>
2021-12-11 15:13:48 -0400

    :sparkles: [ADD] bgSync

--- workbox-config.js
+++ workbox-config.js
@@ -4,8 +4,9 @@ module.exports = {
 		'**/*.{json,ico,html,png,txt,css,js}'
 	],
 	swDest: 'build/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+	swSrc: 'src/sw-template.js', // generateSW no funciona con esta propiedad
+	// ignoreURLParametersMatching: [
+	// 	/^utm_/,
+	// 	/^fbclid$/
+	// ]
 };
